d9c276ffb40b760de83882572093a5c76eabd8f3
Kyle Levisay <kyleandrewlevi43@outlook.com>
2024-05-15 12:03:34 +0900

    Defined Exercise object model

--- src/main/java/org/hsha/hsha/models/Exercise.java
+++ src/main/java/org/hsha/hsha/models/Exercise.java
@@ -1,4 +1,59 @@
 package org.hsha.hsha.models;
 
 public class Exercise {
+
+    String name;
+    String bodyPart; // body part(s) worked
+    int weightKg;
+    int workoutId; // foreign key for workout table
+    int reps; // reps completed this set
+
+    public Exercise(String name, String bodyPart, int weightKg, int reps) {
+        this.name = name;
+        this.bodyPart = bodyPart;
+        this.weightKg = weightKg;
+        this.reps = reps;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getBodyPart() {
+        return bodyPart;
+    }
+
+    public void setBodyPart(String bodyPart) {
+        this.bodyPart = bodyPart;
+    }
+
+    public int getWeightKg() {
+        return weightKg;
+    }
+
+    public void setWeightKg(int weightKg) {
+        this.weightKg = weightKg;
+    }
+
+    public int getWorkoutId() {
+        return workoutId;
+    }
+
+    public void setWorkoutId(int workoutId) {
+        this.workoutId = workoutId;
+    }
+
+    public int getReps() {
+        return reps;
+    }
+
+    public void setReps(int reps) {
+        this.reps = reps;
+    }
+
+
 }
