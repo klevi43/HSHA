ee909c3d00392ba3baa07289ef312badf11694c3
Kyle Levisay <kyleandrewlevi43@outlook.com>
2024-06-04 19:38:56 +0900

    Started learning Thymeleaf

---
+++ src/main/java/org/hsha/hsha/controllers/ThymeleafController.java
@@ -0,0 +1,16 @@
+package org.hsha.hsha.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping(value="/thymeleaf")
+public class ThymeleafController {
+    @GetMapping("/ex01")
+    public String thymeleaf01(Model model) {
+        model.addAttribute("data", "타임리프 예제 입니다");
+        return "thymeleafEx/thymeleafEx01";
+    }
+}
