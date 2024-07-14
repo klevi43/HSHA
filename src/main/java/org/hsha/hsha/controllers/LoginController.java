3b19cf67dc13a92fbd820959cfe2ea663eccbfa8
Kyle Levisay <kyleandrewlevi43@outlook.com>
2024-07-14 13:10:43 +0900

    Started landing page will begin working on implementing login functionality soon

--- src/main/java/org/hsha/hsha/controllers/LoginController.java
+++ src/main/java/org/hsha/hsha/controllers/LoginController.java
@@ -6,7 +6,12 @@ import org.springframework.web.bind.annotation.GetMapping;
 @Controller
 public class LoginController {
 
+    @GetMapping("/references")
+    public String getReferencePage() {
+        return "references/reference-landing";
+    }
     @GetMapping("/")
     public String getHomePage() {
-        return "homepage";    }
+        return "homepage/homepage";
+    }
 }
